af4a4d8d2b67c9f1b3afdc1620f15119e172effb
Jasmeet107 <jasmeet@mit.edu>
2013-11-03 16:50:50 -0800

    Added circles and planes lists and made them reflective"

--- BWR.py
+++ BWR.py
@@ -6,3 +6,15 @@ import openmoc.materialize as materialize
 materials = materialize.materialize('BWR_materials.hdf5')
 print materials
 print len(materials)
+
+circles = [] 
+planes = []
+
+planes.append(XPlane(x=-0.8))
+planes.append(XPlane(x=0.8))
+planes.append(YPlane(y=-0.8))
+planes.append(YPlane(y=0.8))
+circles.append(Circle(x=0.0, y=0.0, radius=0.6))
+circles.append(Circle(x=0.0, y=0.0, radius=0.5))
+for plane in planes:plane.setBoundaryType(REFLECTIVE)
+
