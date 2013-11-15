c784ebc74434ea5016f5e33a1dea0214c52b713a
dvtranmit <dvtran@mit.edu>
2013-11-14 22:22:04 -0500

    Tables fixed. Should be able to push now. Allow about 5 minutes for the simulation to run.

--- BWR.py
+++ BWR.py
@@ -65,9 +65,172 @@ for plane in planes:plane.setBoundaryType(REFLECTIVE)
 #############################   Creating Cells   ##############################
 ###############################################################################
 
-log.py_printf("Normal", "Creating cells...")
+#Code currently changes FSR to test delta k
 
-#creates cells that contain materials (square, ring, circle cells)
+ring_values = [2, 3, 4, 5]
+sector_values = [2, 4, 8, 16]
+k_fsr = []
+
+for r in ring_values:
+    for s in sector_values:
+
+        log.py_printf("Normal", "Creating cells...")
+
+        #creates cells that contain materials (square, ring, circle cells)
+        cells = []
+        cells.append(CellBasic(universe=1, material=uo2_id, rings=r, sectors=s))
+        cells.append(CellBasic(universe=1, material=clad_id, sectors=s))
+        cells.append(CellBasic(universe=1, material=moderator_id, sectors=s))
+        cells.append(CellBasic(universe=2, material=gd2o3_id, rings=r, sectors=s)) 
+        cells.append(CellBasic(universe=2, material=clad_id, sectors=s))
+        cells.append(CellBasic(universe=2, material=moderator_id, sectors=s))
+
+        #creates cells that are filled by the lattice universe
+        cells.append(CellFill(universe=0, universe_fill=3))
+
+        ## Fuel Type: UO2: Universe=1 ##
+
+        #first cell, region within small circle
+        cells[0].addSurface(halfspace=-1, surface=circles[1])
+
+        #second cell, region inside big and outside small circle
+        cells[1].addSurface(halfspace=-1, surface=circles[0])
+        cells[1].addSurface(halfspace=+1, surface=circles[1])
+
+        #third cell, region outside the big circle; the square formed by the four planes 
+        #will be taken care of with the lattice
+        #cells[2].addSurface(halfspace=+1, surface=planes[0])
+        #cells[2].addSurface(halfspace=+1, surface=planes[2])
+        #cells[2].addSurface(halfspace=-1, surface=planes[1])
+        #cells[2].addSurface(halfspace=-1, surface=planes[3])
+        cells[2].addSurface(halfspace=+1, surface=circles[0])
+
+
+
+        ## Fuel Type: UO2 + GD2O3: Universe=2 ##
+
+
+        #fourth cell, region within small circle
+        cells[3].addSurface(halfspace=-1, surface=circles[1])
+
+        #fifth cell, region inside big and outside small circle
+        cells[4].addSurface(halfspace=-1, surface=circles[0])
+        cells[4].addSurface(halfspace=+1, surface=circles[1])
+
+        #sixth cell, region outside the big circle; the square formed by the four planes
+        #will be taken care of with the lattice
+
+        #cells[5].addSurface(halfspace=+1, surface=planes[0])
+        #cells[5].addSurface(halfspace=+1, surface=planes[2])
+        #cells[5].addSurface(halfspace=-1, surface=planes[1])
+        #cells[5].addSurface(halfspace=-1, surface=planes[3])
+        cells[5].addSurface(halfspace=+1, surface=circles[0])
+
+        #seventh cell, giant cell
+        #this takes care of the big square surrounding the entire system
+        cells[6].addSurface(halfspace=+1, surface=planes[0])
+        cells[6].addSurface(halfspace=+1, surface=planes[2])
+        cells[6].addSurface(halfspace=-1, surface=planes[1])
+        cells[6].addSurface(halfspace=-1, surface=planes[3])
+
+        ###############################################################################
+        ###########################   Creating Lattices   #############################
+        ###############################################################################
+
+        log.py_printf('NORMAL', 'Creating simple 4x4 lattice...')
+
+        """A universe is a space containing a fuel pin within our 4x4 lattice. Further 
+        comments below on how the lattice was created."""
+
+        lattice = Lattice(id=3, width_x=1.6, width_y=1.6)
+        """This lattice can be considered our third universe, 1 and 2 being the UO2 and 
+        gd2o3 fuel pins respectively. This is why lattice has an ID of 3. Width 
+        and Height of each little square are taken from the diagram provided with the 
+        data."""
+
+        lattice.setLatticeCells([[1, 1, 1, 1],
+                                 [1, 2, 1, 1],
+                                 [1, 1, 2, 1],
+                                 [1, 1, 1, 1]])
+
+        """Each 1 represents a UO2 fuel pin cell, and each 2 represents a gd2o3 fuel pin 
+        cell in our lattice."""
+
+        ###############################################################################
+        ##########################   Creating the Geometry   ##########################
+        ###############################################################################
+
+        log.py_printf('NORMAL', 'Creating geometry...')
+
+        geometry = Geometry() 
+        """Creates an instance of the Geometry class. This is a 
+        class in the openmoc file."""
+
+        for material in materials.values(): geometry.addMaterial(material)
+        """This one line has a long explanation. Earlier in this file, we ran our
+        materials data through the materialize function and assigned that dictionary
+        to the variable "materials". That means "materials" is a dictionary containing
+        each material as the key, and its attributes (sigma_a, sigma_s, etc...) as 
+        values. The .values() operator returns a list of all the values in the 
+        materials dictionary. The for loop loops through each material in the list
+        and adds that material to the geometry. One confusing thing here is that
+        after the colon, the block of code isn't indented below the for loop--it 
+        continues on the same line. It would function the same if it were written
+        like this:
+
+        for material in materials.values():
+            geometry.addMaterial(material)
+
+        """
+
+        for cell in cells: geometry.addCell(cell)
+        """Same deal here, except cells is a list of our cell types (we have 6). The
+        for loop runs through the cell list and adds each cell to the geometry 
+        (which is, once again, an instance of the Geometry class). The geometry class
+        now understands what each universe in the lattice means so that adding
+        the lattice in the next line makes sense to the geometry."""
+
+        geometry.addLattice(lattice)
+        """Adds the lattice we just created to the geometry."""
+
+        geometry.initializeFlatSourceRegions()
+        """Once the geometry attributes are set up, this method returns
+        "_openmoc.Geometry_initializeFlatSourceRegions(self)" This figures out what each
+        flat source region is in the geometry and gives each one a unique ID"""
+
+        ###############################################################################
+        ########################   Creating the TrackGenerator   ######################
+        ###############################################################################
+
+        #The following runs the simulation for changes in FSR
+
+        log.py_printf('NORMAL', 'Initializing the track generator...')
+
+        #Creates an instance of the TrackGenerator class, takes three parameters
+        track_generator = TrackGenerator(geometry, num_azim, track_spacing)
+        #Runs the generateTracks() method of the TrackGenerator class
+        track_generator.generateTracks()
+
+        ###############################################################################
+        #########################   Running a Simulation ##############################
+        ###############################################################################
+
+        #Creates an instance of the ThreadPrivateSolver class with two parameters
+        solver = ThreadPrivateSolver(geometry, track_generator)
+        #Sets the number of threads with the number imported from options
+        solver.setNumThreads(num_threads)
+        #sets the convergence threshold with tolerance imported from options
+        solver.setSourceConvergenceThreshold(tolerance)
+        #This is where the simulation is actually run. max_iters here is the 
+        #number of iterations for the simulation.
+        solver.convergeSource(max_iters)
+        #Prints a report with time elapsed 
+        solver.printTimerReport()
+        
+        k_fsr.append(solver.getKeff())
+
+
+#Resets cells to default FSR values for the other two tables
 cells = []
 cells.append(CellBasic(universe=1, material=uo2_id, rings=3, sectors=16))
 cells.append(CellBasic(universe=1, material=clad_id, sectors=16))
@@ -76,133 +239,20 @@ cells.append(CellBasic(universe=2, material=gd2o3_id, rings=3, sectors=16))
 cells.append(CellBasic(universe=2, material=clad_id, sectors=16))
 cells.append(CellBasic(universe=2, material=moderator_id, sectors=16))
 
-#creates cells that are filled by the lattice universe
-cells.append(CellFill(universe=0, universe_fill=3))
+k_az = []
+k_ts = []
 
-## Fuel Type: UO2: Universe=1 ##
-
-#first cell, region within small circle
-cells[0].addSurface(halfspace=-1, surface=circles[1])
-
-#second cell, region inside big and outside small circle
-cells[1].addSurface(halfspace=-1, surface=circles[0])
-cells[1].addSurface(halfspace=+1, surface=circles[1])
-
-#third cell, region outside the big circle; the square formed by the four planes 
-#will be taken care of with the lattice
-#cells[2].addSurface(halfspace=+1, surface=planes[0])
-#cells[2].addSurface(halfspace=+1, surface=planes[2])
-#cells[2].addSurface(halfspace=-1, surface=planes[1])
-#cells[2].addSurface(halfspace=-1, surface=planes[3])
-cells[2].addSurface(halfspace=+1, surface=circles[0])
-
-## Fuel Type: UO2 + GD2O3: Universe=2 ##
-
-
-#fourth cell, region within small circle
-cells[3].addSurface(halfspace=-1, surface=circles[1])
-
-#fifth cell, region inside big and outside small circle
-cells[4].addSurface(halfspace=-1, surface=circles[0])
-cells[4].addSurface(halfspace=+1, surface=circles[1])
-
-#sixth cell, region outside the big circle; the square formed by the four planes
-#will be taken care of with the lattice
-
-#cells[5].addSurface(halfspace=+1, surface=planes[0])
-#cells[5].addSurface(halfspace=+1, surface=planes[2])
-#cells[5].addSurface(halfspace=-1, surface=planes[1])
-#cells[5].addSurface(halfspace=-1, surface=planes[3])
-cells[5].addSurface(halfspace=+1, surface=circles[0])
-
-#seventh cell, giant cell
-#this takes care of the big square surrounding the entire system
-cells[6].addSurface(halfspace=+1, surface=planes[0])
-cells[6].addSurface(halfspace=+1, surface=planes[2])
-cells[6].addSurface(halfspace=-1, surface=planes[1])
-cells[6].addSurface(halfspace=-1, surface=planes[3])
-
-###############################################################################
-###########################   Creating Lattices   #############################
-###############################################################################
-
-log.py_printf('NORMAL', 'Creating simple 4x4 lattice...')
-
-"""A universe is a space containing a fuel pin within our 4x4 lattice. Further 
-comments below on how I lattice was created."""
-
-lattice = Lattice(id=3, width_x=1.6, width_y=1.6)
-"""This lattice can be considered our third universe, 1 and 2 being the UO2 and 
-gd2o3 fuel pins respectively. This is why lattice has an ID of 3. Width 
-and Height of each little square are taken from the diagram provided with the 
-data"""
-
-lattice.setLatticeCells([[1, 1, 1, 1],
-                         [1, 2, 1, 1],
-                         [1, 1, 2, 1],
-                         [1, 1, 1, 1]])
-
-"""Each 1 represents a UO2 fuel pin cell, and each 2 represents a gd2o3 fuel pin 
-cell in our lattice."""
-
-###############################################################################
-##########################   Creating the Geometry   ##########################
-###############################################################################
-
-log.py_printf('NORMAL', 'Creating geometry...')
-
-geometry = Geometry() 
-"""Creates an instance of the Geometry class. This is a 
-class in the openmoc folder."""
-
-for material in materials.values(): geometry.addMaterial(material)
-"""This one line has a long explanation. Earlier in this file, we ran our
-materials data through the materialize function and assigned that dictionary
-to the variable "materials". That means "materials" is a dictionary containing
-each material as the key, and its attributes (sigma_a, sigma_s, etc...) as 
-values. The .values() operator returns a list of all the values in the 
-materials dictionary. The for loop loops through each material in the list
-and adds that material to the geometry. One confusing thing here is that
-after the colon, the block of code isn't indented below the for loop--it 
-continues on the same line. It would function the same if it were written
-like this:
-
-for material in materials.values():
-    geometry.addMaterial(material)
-
-"""
-
-for cell in cells: geometry.addCell(cell)
-"""Same deal here, except cells is a list of our cell types (we have 6). The
-for loop runs through the cell list and adds each cell to the geometry 
-(which is, once again, an instance of the Geometry class). The geometry class
-now understands what each universe in the lattice means so that adding
-the lattice in the next line makes sense to the geometry."""
-
-geometry.addLattice(lattice)
-"""Adds the lattice we just created to the geometry."""
-
-geometry.initializeFlatSourceRegions()
-"""Once the geometry attributes are set up, this method returns
-"_openmoc.Geometry_initializeFlatSourceRegions(self)" This figures out what each
-flat source region is in the geometry and gives each one a unique ID"""
-
-###############################################################################
-########################   Creating the TrackGenerator   ######################
-###############################################################################
-
-#k_az = []
 #maxpin_az = []
 #avgpin_az = []
-#az_values = [4, 8, 16, 32, 64, 128]
+az_values = [4, 8, 16, 32, 64, 128, 256]
+ts_values = [0.5, 0.1, 0.05, 0.01]
 
-#for num_azim in az_values:
-
+for num_azim in az_values:
 
     log.py_printf('NORMAL', 'Initializing the track generator...')
 
     #Creates an instance of the TrackGenerator class, takes three parameters
-    track_generator =TrackGenerator(geometry, num_azim, 0.1 """track_spacing""")
+    track_generator =TrackGenerator(geometry, num_azim, 0.1) #track_spacing
     #Runs the generateTracks() method of the TrackGenerator class
     track_generator.generateTracks()
 
@@ -222,9 +272,65 @@ flat source region is in the geometry and gives each one a unique ID"""
     #Prints a report with time elapsed 
     solver.printTimerReport()
     
-    #k_az.append(k_eff)
-#print k_az 
+    k_az.append(solver.getKeff())
+
+for track_spacing in ts_values:
+
+    log.py_printf('NORMAL', 'Initializing the track generator...')
+
+    #Creates an instance of the TrackGenerator class, takes three parameters
+    track_generator = TrackGenerator(geometry, 128, track_spacing) #num_azim
+    #Runs the generateTracks() method of the TrackGenerator class
+    track_generator.generateTracks()
+
+    ###############################################################################
+    #########################   Running a Simulation ##############################
+    ###############################################################################
+
+    #Creates an instance of the ThreadPrivateSolver class with two parameters
+    solver = ThreadPrivateSolver(geometry, track_generator)
+    #Sets the number of threads with the number imported from options
+    solver.setNumThreads(num_threads)
+    #sets the convergence threshold with tolerance imported from options
+    solver.setSourceConvergenceThreshold(tolerance)
+    #This is where the simulation is actually run. max_iters here is the 
+    #number of iterations for the simulation.
+    solver.convergeSource(max_iters)
+    #Prints a report with time elapsed 
+    solver.printTimerReport()
     
+    k_ts.append(solver.getKeff())
+
+counter = 0
+for keff in k_az:   
+    log.py_printf('NORMAL', 'num_azim = ' + str(az_values[counter]) + ' k-eff = ' + str(keff))
+    log.py_printf('NORMAL', 'deltaK = ' + str(keff - 0.986561))
+    log.py_printf('NORMAL', ' ')
+    counter += 1
+
+counter = 0
+for keff in k_ts:    
+    log.py_printf('NORMAL', 'track_spacing = ' + str(ts_values[counter]) + ' k-eff = ' + str(keff))
+    log.py_printf('NORMAL', 'deltaK = ' + str(keff - 0.986561))
+    log.py_printf('NORMAL', ' ')
+    counter += 1
+
+counterR = 0
+counterS = 0
+for keff in k_fsr:  
+    if counterS > 3:                
+        counterR += 1
+        counterS = 0
+        log.py_printf('NORMAL', 'rings = ' + str(ring_values[counterR]) + ' sectors = ' + str(sector_values[counterS]) + ' k-eff = ' + str(keff))
+        log.py_printf('NORMAL', 'deltaK = ' + str(keff - 0.986561))
+        log.py_printf('NORMAL', ' ')
+        counterS += 1  
+    else:
+        log.py_printf('NORMAL', 'rings = ' + str(ring_values[counterR]) + ' sectors = ' + str(sector_values[counterS]) + ' k-eff = ' + str(keff))
+        log.py_printf('NORMAL', 'deltaK = ' + str(keff - 0.986561))
+        log.py_printf('NORMAL', ' ')
+        counterS +=1          
+            
 ###############################################################################
 ############################   Generating Plots   #############################
 ###############################################################################
