35db3724113a2a9d8fcf3a74f6fd394abb4c37e5
spavlick <spavlick@mit.edu>
2013-11-24 18:55:31 -0500

    Calculated pin powers

--- BWR.py
+++ BWR.py
@@ -5,6 +5,9 @@ from openmoc import *
 import openmoc.log as log # this module stores data printed during simulation
 import openmoc.plotter as plotter
 import openmoc.materialize as materialize
+import openmoc.process as process
+import h5py
+import numpy as np
 
 ###############################################################################
 #######################   Main Simulation Parameters   ########################
@@ -242,12 +245,31 @@ cells.append(CellBasic(universe=2, material=moderator_id, sectors=16))
 k_az = []
 k_ts = []
 
-#maxpin_az = []
-#avgpin_az = []
+
 az_values = [4, 8, 16, 32, 64, 128, 256]
 ts_values = [0.5, 0.1, 0.05, 0.01]
+maxpin_az = np.zeros(len(az_values))
+avgpin_az = np.zeros(len(az_values))
+maxpin_ts = np.zeros(len(ts_values))
+avgpin_ts = np.zeros(len(ts_values))
 
-for num_azim in az_values:
+
+def computeMaxAndAvgPinPowers(solver, geometry):
+    process.computeFSRPinPowers(solver, geometry, use_hdf5=True)
+#    return 0, 0
+    actualPinPowers = np.array([[1, 2, 3, 4], 
+                               [5, 6, 7, 8], 
+                               [9, 10, 11, 12], 
+                               [13, 14, 15, 16]])
+        
+    f = h5py.File('pin-powers/fission-rates.hdf5', 'r') 
+    calculatedPinPowers = f['universe0']['fission-rates'][...]
+    normalizedPinPowers = calculatedPinPowers/np.sum(calculatedPinPowers)
+    pinError = (normalizedPinPowers - actualPinPowers) / actualPinPowers
+    f.close()
+    return np.max(pinError), np.mean(pinError)
+
+for index, num_azim in enumerate(az_values):
 
     log.py_printf('NORMAL', 'Initializing the track generator...')
 
@@ -271,10 +293,14 @@ for num_azim in az_values:
     solver.convergeSource(max_iters)
     #Prints a report with time elapsed 
     solver.printTimerReport()
+
+    maxpin_az[index], avgpin_az[index] = computeMaxAndAvgPinPowers(solver, geometry)
     
+        
     k_az.append(solver.getKeff())
 
-for track_spacing in ts_values:
+
+for index, track_spacing in enumerate(ts_values):
 
     log.py_printf('NORMAL', 'Initializing the track generator...')
 
@@ -298,7 +324,9 @@ for track_spacing in ts_values:
     solver.convergeSource(max_iters)
     #Prints a report with time elapsed 
     solver.printTimerReport()
-    
+
+    maxpin_ts[index], avgpin_ts[index] = computeMaxAndAvgPinPowers(solver, geometry)
+  
     k_ts.append(solver.getKeff())
 
 counter = 0
